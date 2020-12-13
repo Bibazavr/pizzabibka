cb20f2c1b880871f023e2f2d409b806760fe2daf
Bibazavr <lirik.hariev@gmail.com>
2020-12-13 14:13:04 +0300

    dell test

--- frontend/src/components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import App from './App';
-
-test('renders Pizzabibka', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Pizzabibka/i);
-  expect(linkElement).toBeInTheDocument();
-});
